6a8800c59bad6a2bf46618920983ed65b6a1239a
Alexandre Malaquin <alexandre.malaquin@epitech.eu>
2020-05-28 17:44:52 +0200

    [+] Add MainMenu, JoinServer and GameScene | Add a pause menu in game | Add 3D Assets for every gems and for team character with a script to spawn them randomely | Start connection with server

---
+++ graphic/Zappy/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float CameraSpeed = 20f;
+    private Vector3 pos;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    float MouseSpeed = 150f;
+
+    public CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRotation = Camera.main.transform.eulerAngles.x;
+        yRotation = Camera.main.transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float MouseX = Input.GetAxis("Mouse X") * MouseSpeed * Time.deltaTime;
+            float MouseY = Input.GetAxis("Mouse Y") * MouseSpeed * Time.deltaTime;
+
+            xRotation -= MouseY;
+            yRotation += MouseX;
+            
+
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * CameraSpeed * Time.deltaTime);
+    }
+}
---
+++ graphic/Zappy/Assets/Scripts/ServerJoin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+public class ServerJoin : MonoBehaviour
+{
+
+    public TMP_InputField IpField;
+    public TMP_InputField PortField;
+
+    private string ip;
+    private string port;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateIp()
+    {
+        ip = IpField.text;
+
+        Debug.Log(ip);
+    }
+
+    public void UpdatePort()
+    {
+        port = PortField.text;
+
+        Debug.Log(port);
+    }
+}
