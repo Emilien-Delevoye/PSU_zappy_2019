2bcdc27e145abaf45892518a08c0ef9efd84c02b
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-29 14:21:20 +0200

    Add basic source files

---
+++ server/include/server.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** server
+** File description:
+** Created by emilien
+*/
+
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+#endif //SERVER_SERVER_H
---
+++ server/src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** server
+** File description:
+** Created by emilien
+*/
+
+#include <stdio.h>
+
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
+{
+    puts("Serveur zappy");
+}
