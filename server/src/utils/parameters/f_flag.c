26acfd42d2e9be805191003a087cec82eceac940
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-29 16:45:02 +0200

    Add parsing for the frequency (f_flag function)

--- server/src/utils/parameters/f_flag.c
+++ server/src/utils/parameters/f_flag.c
@@ -7,11 +7,28 @@
 
 #include "utils/parameters.h"
 #include <string.h>
+#include <stdio.h>
+
+static int get_freq(char *freq_in)
+{
+    int freq;
+
+    if (sscanf(freq_in, "%d", &freq) != 1)
+        freq = -1;
+    return (freq);
+}
 
 bool f_flag(char **av, data_server_t *data)
 {
-    if (strcmp("-f", av[0]) == 0)
+    static bool use_flag = false;
+
+    if (strcmp("-f", av[0]) != 0)
         return (true);
-    (void)data;
+    if (use_flag == true || !av[1])
+        return (false);
+    use_flag = true;
+    data->freq = get_freq(av[1]);
+    if (data->freq < 1)
+        return (false);
     return (true);
 }
