8a1ce3705bf70b490098870b46e54bd6cae7fbe5
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-29 17:36:37 +0200

    Add missing param check

--- server/src/utils/parameters/get_parameters.c
+++ server/src/utils/parameters/get_parameters.c
@@ -22,6 +22,9 @@ data_server_t get_parameters(int ac, char **av)
     for (int a = 0; a < ac && av[a]; ++a)
         for (int b = 0; fl[b]; ++b)
             st = (fl[b](av + a, &data) == true && st == true ? true : false);
+    if (data.team_nb < 1 || !data.team_names || data.port < 1 || data.width < 1
+        || data.height < 1 || data.client_nb || data.freq < 1)
+        st = false;
     data.valid_params = st;
     return (data);
 }
