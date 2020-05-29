2a76b58b86cb7c581ae73bb5e37b86ab647a5d2c
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-29 15:09:17 +0200

    Add data_server_s struct with server settings and server socket

--- server/include/server.h
+++ server/include/server.h
@@ -8,4 +8,16 @@
 #ifndef SERVER_SERVER_H
 #define SERVER_SERVER_H
 
+typedef struct data_server_s {
+    //Server settings
+    int port;
+    unsigned int width;
+    unsigned int height;
+    char **team_names;
+    unsigned short client_nb;
+    unsigned int freq;
+    //Server socket
+    int fd;
+} data_server_t;
+
 #endif //SERVER_SERVER_H
