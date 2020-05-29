b55c52cca92f13d67144cd7dfcf7f7b65a969009
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-29 16:39:27 +0200

    Add parsing for the map width (x_flag function)

--- server/src/utils/parameters/x_flag.c
+++ server/src/utils/parameters/x_flag.c
@@ -7,11 +7,28 @@
 
 #include "utils/parameters.h"
 #include <string.h>
+#include <stdio.h>
+
+static unsigned int get_width(char *width_in)
+{
+    int width;
+
+    if (sscanf(width_in, "%d", &width) != 1)
+        width = -1;
+    return (width);
+}
 
 bool x_flag(char **av, data_server_t *data)
 {
-    if (strcmp("-x", av[0]) == 0)
+    static bool use_flag = false;
+
+    if (strcmp("-x", av[0]) != 0)
         return (true);
-    (void)data;
+    if (use_flag == true || !av[1])
+        return (false);
+    use_flag = true;
+    data->width = get_width(av[1]);
+    if (data->width < 1)
+        return (false);
     return (true);
 }
