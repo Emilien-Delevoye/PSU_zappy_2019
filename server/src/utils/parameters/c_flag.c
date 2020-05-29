0ce7bdd1e79f12c3e1a9b32abeedf38c3f7da694
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-29 16:42:08 +0200

    Add parsing for the clients number (c_flag function)

--- server/src/utils/parameters/c_flag.c
+++ server/src/utils/parameters/c_flag.c
@@ -7,11 +7,28 @@
 
 #include "utils/parameters.h"
 #include <string.h>
+#include <stdio.h>
+
+static unsigned int get_client_nb(char *client_nb_in)
+{
+    int client_nb;
+
+    if (sscanf(client_nb_in, "%d", &client_nb) != 1)
+        client_nb = -1;
+    return (client_nb);
+}
 
 bool c_flag(char **av, data_server_t *data)
 {
-    if (strcmp("-c", av[0]) == 0)
+    static bool use_flag = false;
+
+    if (strcmp("-c", av[0]) != 0)
         return (true);
-    (void)data;
+    if (use_flag == true || !av[1])
+        return (false);
+    use_flag = true;
+    data->client_nb = get_client_nb(av[1]);
+    if (data->client_nb < 1)
+        return (false);
     return (true);
 }
