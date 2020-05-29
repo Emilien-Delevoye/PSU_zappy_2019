296e3621d1bb593bb3a1babf8a876f88e5d7b9d1
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-29 16:40:32 +0200

    Add parsing for the map height (y_flag function)

--- server/src/utils/parameters/y_flag.c
+++ server/src/utils/parameters/y_flag.c
@@ -7,11 +7,28 @@
 
 #include "utils/parameters.h"
 #include <string.h>
+#include <stdio.h>
+
+static unsigned int get_height(char *height_in)
+{
+    int height;
+
+    if (sscanf(height_in, "%d", &height) != 1)
+        height = -1;
+    return (height);
+}
 
 bool y_flag(char **av, data_server_t *data)
 {
-    if (strcmp("-y", av[0]) == 0)
+    static bool use_flag = false;
+
+    if (strcmp("-y", av[0]) != 0)
         return (true);
-    (void)data;
+    if (use_flag == true || !av[1])
+        return (false);
+    use_flag = true;
+    data->height = get_height(av[1]);
+    if (data->height < 1)
+        return (false);
     return (true);
 }
