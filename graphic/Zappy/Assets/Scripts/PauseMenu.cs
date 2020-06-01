b9b685debf3a28d9a6828c6cd54d18185c23d4da
Alexandre Malaquin <alexandre.malaquin@epitech.eu>
2020-06-01 20:23:55 +0200

    [+] Adding sound management and improve option menu

--- graphic/Zappy/Assets/Scripts/PauseMenu.cs
+++ graphic/Zappy/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -19,5 +20,10 @@ public class PauseMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
