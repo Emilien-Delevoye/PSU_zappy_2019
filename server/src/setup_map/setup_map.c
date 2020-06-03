57233bbea38d592b416c24694babcf5de1d7d878
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-03 15:22:54 +0200

    Add setup_map function -> Lines creation (link missing)

--- server/src/setup_map/setup_map.c
+++ server/src/setup_map/setup_map.c
@@ -21,7 +21,25 @@ map_t *create_node(void)
     return (new);
 }
 
-int setup_map(data_server_t *data __attribute__((unused)))
+int setup_map(data_server_t *data)
 {
+    map_t *first = create_node();
+    map_t *cur_start = first;
+    map_t *cur;
+
+    if (!first)
+        return (84);
+    data->top_left = first;
+    for (int a = 1; a < data->height; ++a) {
+        cur = cur_start;
+        for (int b = 1; b < data->width; ++b) {
+            cur->right = create_node();
+            cur->right->left = cur;
+            cur = cur->right;
+        }
+        cur_start->bottom = create_node();
+        cur_start->bottom->top = cur_start;
+        cur_start = cur_start->bottom;
+    }
     return (0);
 }
