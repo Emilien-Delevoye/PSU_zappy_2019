45a6a07932bace528f1636e16656b61dc5092c99
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-05 14:58:49 +0200

    Add add_to_list function for add_to_write_list

--- server/src/utils/write_list/write_list.c
+++ server/src/utils/write_list/write_list.c
@@ -13,15 +13,28 @@ static write_cli_t *create_write_list(char *buffer)
 {
     write_cli_t *new = malloc(sizeof(write_cli_t));
 
+    if (!new)
+        return (NULL);
     new->to_write = strdup(buffer);
     new->to_write = NULL;
     new->next = NULL;
     return (new);
 }
 
+static void add_to_list(client_t *cli, char *buffer)
+{
+    write_cli_t *new = create_write_list(buffer);
+    write_cli_t *cur = cli->list_msg;
+
+    while (cur->next)
+        cur = cur->next;
+    cur->next = new;
+}
+
 void add_to_write_list(client_t *cli, char *buffer)
 {
-    if (!cli->list_msg) {
+    if (!cli->list_msg)
         cli->list_msg = create_write_list(buffer);
-    }
+    else
+        add_to_list(cli, buffer);
 }
