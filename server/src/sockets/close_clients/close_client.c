92afef227a65147a068e7e322943c3d4fa4fd9a0
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-06 19:54:40 +0200

    Add free for command_queue in client struct

--- server/src/sockets/close_clients/close_client.c
+++ server/src/sockets/close_clients/close_client.c
@@ -7,12 +7,23 @@
 
 #include "server.h"
 #include "sockets/select.h"
+#include <stdlib.h>
 
 void close_clients(data_server_t *data)
 {
+    command_queue_t *save = NULL;
+
     for (client_t *cli = data->l_cli.first; cli; cli = cli->next) {
         if (cli->to_close == false)
             continue;
+        for (command_queue_t *c = cli->cmd_queue; c; c = c->next) {
+            if (save) {
+                if (save->command)
+                    free(save->command);
+                free(save);
+            }
+            save = c;
+        }
         remove_a_client(data, cli);
         close_clients(data);
         return;
