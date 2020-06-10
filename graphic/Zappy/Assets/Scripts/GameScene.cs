8b24283a681b2dec092cc3d0242986d064843636
Alexandre Malaquin <alexandre.malaquin@epitech.eu>
2020-06-10 20:47:40 +0200

    Add move function without animation and random pos spawn for characters

--- graphic/Zappy/Assets/Scripts/GameScene.cs
+++ graphic/Zappy/Assets/Scripts/GameScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
 public class GameScene : MonoBehaviour
 {
@@ -19,9 +20,15 @@ public class GameScene : MonoBehaviour
     public GameObject thystame;
     public GameObject food;
 
+
+    public GameObject character;
+    public RuntimeAnimatorController runtimeAnimator;
+    private Animator animator;
+
+
     private Vector3 spawnPos;
 
-    private Vector3 pos;
+    private Vector3 mapPos;
     private Quaternion rotation;
 
     private int mapSizeX = 0;
@@ -33,8 +40,95 @@ public class GameScene : MonoBehaviour
     private string receiveMessage;
     private string[] arguments;
 
-    private LinkedList<GameObject> characters;
     private LinkedList<string> teams;
+
+    public class Character
+    {
+        public Character(GameObject gameObject, int number, int posX, int posY, int orientation, int level, string teamName)
+        {
+            _gameObject = gameObject;
+            _number = number;
+            _posX = posX;
+            _posY = posY;
+            _posToMoveX = posX;
+            _posToMoveY = posY;
+            _orientation = orientation;
+            _level = level;
+            _teamName = teamName;
+            _gameObject.transform.position = new Vector3(UnityEngine.Random.Range(posX + 0.2f, posX + 0.8f), 0, UnityEngine.Random.Range(posY + 0.2f, posY + 0.8f));
+            if (orientation == 1)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            else if (orientation == 2)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+            else if (orientation == 3)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            else if (orientation == 4)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 270f, 0f);
+        }
+
+        public void SetPosition(int posX, int posY)
+        {
+            _posX = posX;
+            _posY = posY;
+            _gameObject.transform.position = new Vector3(_posX, 0, _posY);
+        }
+
+        public void SetPointToMove(int posX, int posY)
+        {
+            _posToMoveX = posX;
+            _posToMoveY = posY;
+            //_gameObject.transform.position += _gameObject.transform.forward * Time.deltaTime;
+            //_gameObject.transform.position = Vector3.MoveTowards(_gameObject.transform.position, new Vector3(posX, 0f, posY), 800f);
+            //_gameObject.transform.Translate(_posX * Time.deltaTime, 0, _posY * Time.deltaTime);
+        }
+
+        public bool NeedToMove()
+        {
+            if (_posToMoveX == _posX && _posToMoveY == _posY)
+                return false;
+            return true;
+           
+        }
+
+        public void Move()
+        {
+            Debug.Log("pos to move: " + _posToMoveY + " pos : " + _posY);
+            if (_posToMoveX/2 >= _gameObject.transform.position.x - 0.5f && _posToMoveX/2 <= _gameObject.transform.position.x + 0.5f)
+            {
+                _posX = _posToMoveX;
+            }
+            if (_posToMoveY/2 >= _gameObject.transform.position.z - 0.5f && _posToMoveY/2 <= _gameObject.transform.position.z + 0.5f)
+            {
+                _posY = _posToMoveY;
+            }
+            _gameObject.transform.Translate(_posToMoveX * Time.deltaTime * 0.5f, 0, _posToMoveY * Time.deltaTime * 0.5f, Space.World);
+        }
+
+        public void SetOrientation(int orientation)
+        {
+            if (orientation == 1)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            else if (orientation == 2)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+            else if (orientation == 3)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            else if (orientation == 4)
+                _gameObject.transform.eulerAngles = new Vector3(0f, 270f, 0f);
+        }
+
+        private GameObject _gameObject;
+        private int _number;
+        private int _posX;
+        private int _posY;
+        private int _posToMoveX;
+        private int _posToMoveY;
+        private int _orientation;
+        private int _level;
+        private string _teamName;
+    };
+    private Dictionary<int, Character> characters;
+
+    static int tmp = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -42,8 +136,12 @@ public class GameScene : MonoBehaviour
         rotation.x = 0f;
         rotation.y = 0f;
         rotation.z = 0f;
-        pos.x = startPointX;
-        pos.z = startPointZ;
+        mapPos.x = startPointX;
+        mapPos.z = startPointZ;
+
+        characters = new Dictionary<int, Character>();
+        animator = character.gameObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = runtimeAnimator;
 
         SetUpAssetsSize();
         SetUpSizeMap();
@@ -54,8 +152,31 @@ public class GameScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        receiveMessage = client.ReceiveMesageFromServer();
+        ReceiveMessageFromServer();
+        foreach (KeyValuePair<int, Character> kvp in characters)
+        {
+            if (characters[kvp.Key].NeedToMove())
+            {
+                characters[kvp.Key].Move();
+            }
+        }
 
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            PauseMenu.SetActive(true);
+        }
+    }
+
+    public void ReceiveMessageFromServer()
+    {
+        receiveMessage = client.ReceiveMesageFromServer();
+        if (tmp == 0)
+            receiveMessage = "pnw 0 0 0 2 3 Team1\n";
+        else if (tmp == 150)
+            receiveMessage = "ppo 0 5 0 2\n";
+        else
+            receiveMessage = null;
+        ++tmp;
         if (receiveMessage != null)
         {
             Array.Clear(arguments, 0, arguments.Length);
@@ -63,18 +184,38 @@ public class GameScene : MonoBehaviour
             if (arguments[0] == "pnw")
             {
                 InstantiateNewPlayer();
+            } else if (arguments[0] == "ppo")
+            {
+                SetPointToMovePlayer();
             }
-            
+
         }
-        if (Input.GetKey(KeyCode.Escape))
+    }
+
+    public void SetPointToMovePlayer()
+    {
+        if (arguments.Length >= 5)
         {
-            //client.SendMessageToServer("PAUSE MENU");
-            PauseMenu.SetActive(true);
+            Debug.Log(int.Parse(arguments[3]));
+            characters[int.Parse(arguments[1])].SetPointToMove(int.Parse(arguments[2]), int.Parse(arguments[3]));
+            characters[int.Parse(arguments[1])].SetOrientation(int.Parse(arguments[4]));
+        } else
+        {
+            Debug.Log("ppo: Reply missing argument.");
         }
     }
 
     public void InstantiateNewPlayer()
     {
+        if (arguments.Length >= 7)
+        {
+            spawnPos.x = UnityEngine.Random.Range(int.Parse(arguments[2]) + 0.2f, int.Parse(arguments[2]) + 0.8f);
+            spawnPos.z = UnityEngine.Random.Range(int.Parse(arguments[3]) + 0.2f, int.Parse(arguments[3]) + 0.8f);
+            characters.Add(int.Parse(arguments[1]), new Character(Instantiate(character), int.Parse(arguments[1]), int.Parse(arguments[2]), int.Parse(arguments[3]), int.Parse(arguments[4]), int.Parse(arguments[5]), arguments[6]));
+        } else
+        {
+            Debug.Log("pnw: Reply missing argument.");
+        }
 
     }
 
@@ -139,12 +280,12 @@ public class GameScene : MonoBehaviour
                 for (int x = 0; x < mapSizeX; x++)
                 {
                     client.SendMessageToServer("bct " + x + " " + y + "\n");
-                    Instantiate(plane, pos, rotation);
+                    Instantiate(plane, mapPos, rotation);
                     SetUpTile();
-                    pos.x += 1f;
+                    mapPos.x += 1f;
                 }
-                pos.z += 1f;
-                pos.x = startPointZ;
+                mapPos.z += 1f;
+                mapPos.x = startPointZ;
             }
         }
         else
@@ -154,12 +295,12 @@ public class GameScene : MonoBehaviour
                 for (int y = 0; y < mapSizeY; y++)
                 {
                     client.SendMessageToServer("bct " + x + " " + y + "\n");
-                    Instantiate(plane, pos, rotation);
+                    Instantiate(plane, mapPos, rotation);
                     SetUpTile();
-                    pos.z += 1f;
+                    mapPos.z += 1f;
                 }
-                pos.x += 1f;
-                pos.z = startPointZ;
+                mapPos.x += 1f;
+                mapPos.z = startPointZ;
             }
         }
     }
@@ -168,11 +309,11 @@ public class GameScene : MonoBehaviour
     {
         Array.Clear(arguments, 0, arguments.Length);
         //receiveMessage = client.WaitMessageFromServer();
-        receiveMessage = "tmp";
+        receiveMessage = "bct 0 0 1 1 1 1 1 1 1\n";
+
         if (receiveMessage != null)
         {
-            //arguments = receiveMessage.Split(' ');
-            arguments = "bct 0 0 1 1 1 1 1 1 1\n".Split(' ');
+            arguments = receiveMessage.Split(' ');
             if (arguments[0] == "bct")
             {
                 if (arguments.Length != 10)
@@ -197,8 +338,8 @@ public class GameScene : MonoBehaviour
     {
         for (int i = 0; i < int.Parse(arguments[3]); i++)
         {
-            spawnPos.x = UnityEngine.Random.Range(pos.x - 0.5f, pos.x + 0.5f);
-            spawnPos.z = UnityEngine.Random.Range(pos.z - 0.5f, pos.z + 0.5f);
+            spawnPos.x = UnityEngine.Random.Range(mapPos.x - 0.5f, mapPos.x + 0.5f);
+            spawnPos.z = UnityEngine.Random.Range(mapPos.z - 0.5f, mapPos.z + 0.5f);
             spawnPos.y = 0.1f;
             Instantiate(food, spawnPos, rotation);
         }
@@ -208,8 +349,8 @@ public class GameScene : MonoBehaviour
     {
         for (int i = 0; i < int.Parse(arguments[4]); i++)
         {
-            spawnPos.x = UnityEngine.Random.Range(pos.x - 0.5f, pos.x + 0.5f);
-            spawnPos.z = UnityEngine.Random.Range(pos.z - 0.5f, pos.z + 0.5f);
+            spawnPos.x = UnityEngine.Random.Range(mapPos.x - 0.5f, mapPos.x + 0.5f);
+            spawnPos.z = UnityEngine.Random.Range(mapPos.z - 0.5f, mapPos.z + 0.5f);
             spawnPos.y = 0.050f;
             Instantiate(linemate, spawnPos, rotation);
         }
@@ -219,8 +360,8 @@ public class GameScene : MonoBehaviour
     {
         for (int i = 0; i < int.Parse(arguments[5]); i++)
         {
-            spawnPos.x = UnityEngine.Random.Range(pos.x - 0.5f, pos.x + 0.5f);
-            spawnPos.z = UnityEngine.Random.Range(pos.z - 0.5f, pos.z + 0.5f);
+            spawnPos.x = UnityEngine.Random.Range(mapPos.x - 0.5f, mapPos.x + 0.5f);
+            spawnPos.z = UnityEngine.Random.Range(mapPos.z - 0.5f, mapPos.z + 0.5f);
             spawnPos.y = 0.050f;
             Instantiate(deraumere, spawnPos, rotation);
         }
@@ -230,8 +371,8 @@ public class GameScene : MonoBehaviour
     {
         for (int i = 0; i < int.Parse(arguments[6]); i++)
         {
-            spawnPos.x = UnityEngine.Random.Range(pos.x - 0.5f, pos.x + 0.5f);
-            spawnPos.z = UnityEngine.Random.Range(pos.z - 0.5f, pos.z + 0.5f);
+            spawnPos.x = UnityEngine.Random.Range(mapPos.x - 0.5f, mapPos.x + 0.5f);
+            spawnPos.z = UnityEngine.Random.Range(mapPos.z - 0.5f, mapPos.z + 0.5f);
             spawnPos.y = 0.050f;
             Instantiate(sibur, spawnPos, rotation);
         }
@@ -241,8 +382,8 @@ public class GameScene : MonoBehaviour
     {
         for (int i = 0; i < int.Parse(arguments[7]); i++)
         {
-            spawnPos.x = UnityEngine.Random.Range(pos.x - 0.5f, pos.x + 0.5f);
-            spawnPos.z = UnityEngine.Random.Range(pos.z - 0.5f, pos.z + 0.5f);
+            spawnPos.x = UnityEngine.Random.Range(mapPos.x - 0.5f, mapPos.x + 0.5f);
+            spawnPos.z = UnityEngine.Random.Range(mapPos.z - 0.5f, mapPos.z + 0.5f);
             spawnPos.y = 0.050f;
             Instantiate(mendiane, spawnPos, rotation);
         }
@@ -252,8 +393,8 @@ public class GameScene : MonoBehaviour
     {
         for (int i = 0; i < int.Parse(arguments[8]); i++)
         {
-            spawnPos.x = UnityEngine.Random.Range(pos.x - 0.5f, pos.x + 0.5f);
-            spawnPos.z = UnityEngine.Random.Range(pos.z - 0.5f, pos.z + 0.5f);
+            spawnPos.x = UnityEngine.Random.Range(mapPos.x - 0.5f, mapPos.x + 0.5f);
+            spawnPos.z = UnityEngine.Random.Range(mapPos.z - 0.5f, mapPos.z + 0.5f);
             spawnPos.y = 0.050f;
             Instantiate(phiras, spawnPos, rotation);
         }
@@ -263,8 +404,8 @@ public class GameScene : MonoBehaviour
     {
         for (int i = 0; i < int.Parse(arguments[9]); i++)
         {
-            spawnPos.x = UnityEngine.Random.Range(pos.x - 0.5f, pos.x + 0.5f);
-            spawnPos.z = UnityEngine.Random.Range(pos.z - 0.5f, pos.z + 0.5f);
+            spawnPos.x = UnityEngine.Random.Range(mapPos.x - 0.5f, mapPos.x + 0.5f);
+            spawnPos.z = UnityEngine.Random.Range(mapPos.z - 0.5f, mapPos.z + 0.5f);
             spawnPos.y = 0.050f;
             Instantiate(thystame, spawnPos, rotation);
         }
