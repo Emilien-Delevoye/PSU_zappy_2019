1000d785067bd5b8964350c6f73144639e6e41fc
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-11 15:13:44 +0200

    Update time unit test with new settings

--- tests/src/time.cpp
+++ tests/src/time.cpp
@@ -14,11 +14,12 @@ TEST(time, time)
     long timer_1;
     long timer_2;
 
+    memset(&data, 0, sizeof(data_server_t));
     setup_timer(&data);
-    timer_1 = data.timer;
+    timer_1 = data.tv.tv_usec;
     usleep(2000);
     timer(&data);
-    timer_2 = data.timer;
+    timer_2 = data.tv.tv_usec;
     std::cout << timer_1 << " && " << timer_2 << std::endl;
     EXPECT_EQ(timer_1 < timer_2, true);
 }
