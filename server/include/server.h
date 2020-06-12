f771adba0734d21d70d205fa5773a4f207404d22
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-12 11:40:23 +0200

    Update list_actions_s structure (move timeval [2] to timeval)

--- server/include/server.h
+++ server/include/server.h
@@ -59,7 +59,7 @@ struct list_client_s {
 
 typedef struct list_actions_s {
     client_t *cli;
-    struct timeval tv[2];
+    struct timeval tv;
     struct list_actions_s *next;
 } list_actions_t;
 
