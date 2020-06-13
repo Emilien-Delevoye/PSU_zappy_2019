89a5f8dac3b1615fe7055413efd2537e9b186bec
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-13 21:46:33 +0200

    Update monitoring information display

--- server/src/ai_interaction/ai_interaction_main.c
+++ server/src/ai_interaction/ai_interaction_main.c
@@ -31,16 +31,23 @@ static void update_waiting_cli(data_server_t *data, struct timeval cur_time,
     }
 }
 
-static void update_time(struct timeval tv[2])
+static void update_time(struct timeval tv[2], unsigned int freq)
 {
     static int performance = 0;
+    double loop_du;
+    double min_du;
 
     tv[0].tv_sec = tv[1].tv_sec;
     tv[0].tv_usec = tv[1].tv_usec;
     gettimeofday(&tv[1], NULL);
     performance++;
     if (tv[0].tv_sec != tv[1].tv_sec) {
-        printf("Perf : %d\n", performance);
+        loop_du = ((1.0 / performance) * 1000000);
+        min_du = (1.0 / freq) * 1000000;
+        printf("Perf : %d tps || %lf loop duration (µs) ||"
+            " %lf min duration (µs)\n", performance, loop_du, min_du);
+        if (loop_du > min_du)
+            printf("WARNING: Server is slow !\n");
         performance = 0;
     }
 }
@@ -49,7 +56,7 @@ void ai_interaction(data_server_t *data)
 {
     static struct timeval tv[2];
 
-    update_time(tv);
+    update_time(tv, data->params.freq);
     update_work_cli(data);
     update_waiting_cli(data, tv[0], data->cli_wait);
 }
