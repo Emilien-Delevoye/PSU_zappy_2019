0b325d97a08ff07e69f7c56160fd1fef9240208f
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-17 18:19:23 +0200

    Add inventory function

--- server/src/ai_interaction/inventory.c
+++ server/src/ai_interaction/inventory.c
@@ -9,5 +9,13 @@
 
 void inventory(data_server_t *data)
 {
-    (void)data;
+    char str[500] = {0};
+    drone_t cur = data->cli_work->cli->drone;
+
+    sprintf(str, "[linemate %d, sibur %d, deraumere %d, mendiane %d,"
+        "phiras %d, thystame %d, food %d\n", cur.inventory[LINEMATE],
+        cur.inventory[SIBUR], cur.inventory[DERAUMERE],
+        cur.inventory[MENDIANE], cur.inventory[PHIRAS],
+        cur.inventory[THYSTAME], cur.inventory[FOOD]);
+    add_to_write_list(data->cli_work->cli, str);
 }
