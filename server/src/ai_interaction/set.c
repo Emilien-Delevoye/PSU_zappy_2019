59f927e7d8118d4d862111835f6cc06ee6d606cf
William <willi.petitprez@gmail.com>
2020-06-19 18:09:29 +0200

    complete set function

--- server/src/ai_interaction/set.c
+++ server/src/ai_interaction/set.c
@@ -6,10 +6,35 @@
 */
 
 #include "server.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *get_set_arg(char *str)
+{
+    char *tmp = malloc(sizeof(char) * strlen(str) - 3);
+
+    memset(tmp, 0, sizeof(char) * strlen(str) - 3);
+    for (int i = 3; str[i]; ++i) {
+        if (i != 3)
+            tmp[i - 4] = str[i];
+    }
+    return tmp;
+}
 
 void set(data_server_t *data)
 {
     client_t *cli = data->cli_work->cli;
+    char *str = get_set_arg(data->cli_work->cmd_str);
+    map_t *cur = cli->drone.tile;
+    char *equi[7] = {"food", "linemate", "deraumere", "sibur", "mendiane",
+        "phiras", "thystame"};
 
-    (void)cli;
+    for (int i = 0; i < 7; ++i) {
+        if (strcmp(equi[i], str) == 0 && cli->drone.inventory[i] > 0) {
+            cur->items[i] += 1;
+            add_to_write_list(cli, "ok\n");
+            return;
+        }
+    }
+    add_to_write_list(cli, "ko\n");
 }
