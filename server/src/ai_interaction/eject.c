92b77af58269960f97539c2e4df0fa4a2da78cf5
William <willi.petitprez@gmail.com>
2020-06-20 10:52:22 +0200

    add the beginning of eject (absolutely not finished but need to push)

--- server/src/ai_interaction/eject.c
+++ server/src/ai_interaction/eject.c
@@ -7,9 +7,22 @@
 
 #include "server.h"
 
+void move_for_eject(client_t *cli, int orientation)
+{
+    char str[20] = {0};
+
+
+}
+
 void eject(data_server_t *data)
 {
     client_t *cli = data->cli_work->cli;
 
+    for (tile_players_t *tmp =
+        cli->drone.tile->list_players; tmp; tmp = tmp->next) {
+        if (tmp->cli != cli) {
+            move_for_eject(tmp->cli, cli->drone.orientation);
+        }
+    }
     (void)cli;
 }
