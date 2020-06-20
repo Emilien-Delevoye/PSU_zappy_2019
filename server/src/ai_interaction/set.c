f43cfd47c256e71d4109b2d9bb09634b5be34440
William <willi.petitprez@gmail.com>
2020-06-20 16:56:35 +0200

    add call to bct_command in take and set function

--- server/src/ai_interaction/set.c
+++ server/src/ai_interaction/set.c
@@ -6,6 +6,8 @@
 */
 
 #include "server.h"
+#include "lib.h"
+#include "commands/commands.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -23,6 +25,7 @@ char *get_set_arg(char *str)
 
 void set(data_server_t *data)
 {
+    char arg[30] = {0};
     client_t *cli = data->cli_work->cli;
     char *str = get_set_arg(data->cli_work->cmd_str);
     map_t *cur = cli->drone.tile;
@@ -34,6 +37,8 @@ void set(data_server_t *data)
             cur->items[i] += 1;
             cli->drone.inventory[i] -= 1;
             add_to_write_list(cli, "ok\n");
+            sprintf(arg, "bct %d %d", cur->coord[0], cur->coord[1]);
+            bct_command(cli, data, my_str_to_word_array(arg));
             return;
         }
     }
--- server/src/ai_interaction/take.c
+++ server/src/ai_interaction/take.c
@@ -6,6 +6,8 @@
 */
 
 #include "server.h"
+#include "commands/commands.h"
+#include "lib.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -36,6 +38,7 @@ void spawn_random_food(data_server_t *data, int value)
 
 void take(data_server_t *data)
 {
+    char arg[30] = {0};
     client_t *cli = data->cli_work->cli;
     char *str = get_take_arg(data->cli_work->cmd_str);
     map_t *cur = cli->drone.tile;
@@ -48,6 +51,8 @@ void take(data_server_t *data)
             cur->items[i] -= 1;
             cli->drone.inventory[i] += 1;
             add_to_write_list(cli, "ok\n");
+            sprintf(arg, "bct %d %d", cur->coord[0], cur->coord[1]);
+            bct_command(cli, data, my_str_to_word_array(arg));
             return;
         }
     }
