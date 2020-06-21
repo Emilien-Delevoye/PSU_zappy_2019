381a12a944c8450949162b4642f14bde7a2b4925
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 22:40:31 +0200

    Add pin_command function test

--- tests/src/pbc_command.cpp
+++ tests/src/pbc_command.cpp
@@ -69,3 +69,20 @@ TEST(pie_command, classic_test)
     EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
         "pie 3 0 0 a\n");
 }
+
+TEST(pin_command, classic_test)
+{
+    data_server_t data{};
+    client_t cli{};
+
+    data.l_graphical.first = new client_t;
+    memset(data.l_graphical.first, 0, sizeof(client_t));
+    memset(&cli, 0, sizeof(client_t));
+    cli.drone.tile = new map_t;
+    cli.drone.tile->coord[0] = 0;
+    cli.drone.tile->coord[1] = 0;
+    cli.drone.id = 3;
+    pin_command(&cli, &data);
+    EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
+        "pin 3 0 0 0 0 0 0 0\n");
+}
