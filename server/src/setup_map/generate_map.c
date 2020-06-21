22f3732715f3f4d58b3d0315c71a8ef9aba255f9
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 20:02:56 +0200

    Update food generation

--- server/src/setup_map/generate_map.c
+++ server/src/setup_map/generate_map.c
@@ -12,7 +12,7 @@ void generate_stone_on_one_case(map_t *map)
 {
     for (int i = 0; i < 7; ++i) {
         if (i == FOOD)
-            map->items[i] = rand() % 5;
+            map->items[i] = rand() % 5 + 1;
         else
             map->items[i] = rand() % 2;
     }
