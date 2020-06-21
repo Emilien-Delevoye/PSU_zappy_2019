58fc14f4ba23f15447636a488700fa27ab22138e
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 22:52:15 +0200

    Add sgt_command function test

--- tests/src/pbc_command.cpp
+++ tests/src/pbc_command.cpp
@@ -160,3 +160,22 @@ TEST(seg_command, classic_test)
     EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
         "seg Team1\n");
 }
+
+TEST(sgt_command, classic_test)
+{
+    data_server_t data{};
+    client_t cli{};
+
+    data.l_graphical.first = new client_t;
+    memset(data.l_graphical.first, 0, sizeof(client_t));
+    memset(&cli, 0, sizeof(client_t));
+    cli.drone.tile = new map_t;
+    cli.drone.tile->coord[0] = 0;
+    cli.drone.tile->coord[1] = 0;
+    cli.drone.id = 3;
+    cli.drone.orientation = 3;
+    data.params.freq = 40;
+    sgt_command(&cli, &data, nullptr);
+    EXPECT_EQ(std::string(cli.list_msg->to_write),
+        "sgt 40\n");
+}
