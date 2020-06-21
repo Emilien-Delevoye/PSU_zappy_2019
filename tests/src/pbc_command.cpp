9fab094df9dbfa57cb220c6cf3a8705456b7ed7e
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 22:34:49 +0200

    Add pdi_command function test

--- tests/src/pbc_command.cpp
+++ tests/src/pbc_command.cpp
@@ -21,3 +21,17 @@ TEST(pbc_command, classic_test)
     EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
         "pbc 3\n");
 }
+
+TEST(pdi_command, classic_test)
+{
+    data_server_t data{};
+    client_t cli{};
+
+    data.l_graphical.first = new client_t;
+    memset(data.l_graphical.first, 0, sizeof(client_t));
+    memset(&cli, 0, sizeof(client_t));
+    cli.drone.id = 3;
+    pdi_command(&cli, &data);
+    EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
+        "pdi 3\n");
+}
