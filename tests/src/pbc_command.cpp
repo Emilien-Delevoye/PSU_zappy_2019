aea79ce418d1f984a2d62e7ecc951d2b4dff4632
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 22:47:26 +0200

    Add ppo_command function test

--- tests/src/pbc_command.cpp
+++ tests/src/pbc_command.cpp
@@ -122,3 +122,21 @@ TEST(pnw_command, classic_test)
     EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
         "pnw 3 0 0 0 0 Team1\n");
 }
+
+TEST(ppo_command, classic_test)
+{
+    data_server_t data{};
+    client_t cli{};
+
+    data.l_graphical.first = new client_t;
+    memset(data.l_graphical.first, 0, sizeof(client_t));
+    memset(&cli, 0, sizeof(client_t));
+    cli.drone.tile = new map_t;
+    cli.drone.tile->coord[0] = 0;
+    cli.drone.tile->coord[1] = 0;
+    cli.drone.id = 3;
+    cli.drone.orientation = 3;
+    ppo_command(&cli, &data);
+    EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
+        "ppo 3 0 0 4\n");
+}
