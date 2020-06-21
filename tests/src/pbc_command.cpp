fa6d2f650aea2bc7af300cf258f22e2c9aac7b90
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 22:37:27 +0200

    Add pic_command function test

--- tests/src/pbc_command.cpp
+++ tests/src/pbc_command.cpp
@@ -35,3 +35,20 @@ TEST(pdi_command, classic_test)
     EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
         "pdi 3\n");
 }
+
+TEST(pic_command, classic_test)
+{
+    data_server_t data{};
+    client_t cli{};
+
+    data.l_graphical.first = new client_t;
+    memset(data.l_graphical.first, 0, sizeof(client_t));
+    memset(&cli, 0, sizeof(client_t));
+    cli.drone.tile = new map_t;
+    cli.drone.tile->coord[0] = 0;
+    cli.drone.tile->coord[1] = 0;
+    cli.drone.id = 3;
+    pic_command(&cli, &data);
+    EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
+        "pic 0 0 0 3 3\n");
+}
