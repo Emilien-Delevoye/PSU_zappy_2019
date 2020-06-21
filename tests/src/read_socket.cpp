b82ab32e5dc87bd8153a7145c0f2c05ae98d1536
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 12:23:31 +0200

    Update unit tests after egg management addition

--- tests/src/read_socket.cpp
+++ tests/src/read_socket.cpp
@@ -18,6 +18,7 @@ TEST(read_socket, read_socket)
     if (fd < 0)
         return;
     memset(&data, 0, sizeof(data_server_t));
+    data.params.egg_r_c = new unsigned short[5]{0, 0, 0, 0, 0};
     FD_SET(fd, &data.fdset_read);
     add_client_to_list(&data, fd);
     add_client_to_list(&data, fd);
