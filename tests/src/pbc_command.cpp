0f46b96761b7e1931f385037a70c8e5acb500c69
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-06-21 22:49:57 +0200

    Add seg_command function test

--- tests/src/pbc_command.cpp
+++ tests/src/pbc_command.cpp
@@ -142,3 +142,21 @@ TEST(ppo_command, classic_test)
     EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
         "ppo 3 0 0 4\n");
 }
+
+TEST(seg_command, classic_test)
+{
+    data_server_t data{};
+    client_t cli{};
+
+    data.l_graphical.first = new client_t;
+    memset(data.l_graphical.first, 0, sizeof(client_t));
+    memset(&cli, 0, sizeof(client_t));
+    cli.drone.tile = new map_t;
+    cli.drone.tile->coord[0] = 0;
+    cli.drone.tile->coord[1] = 0;
+    cli.drone.id = 3;
+    cli.drone.orientation = 3;
+    seg_command(&data, (char *)"Team1");
+    EXPECT_EQ(std::string(data.l_graphical.first->list_msg->to_write),
+        "seg Team1\n");
+}
